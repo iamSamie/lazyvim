ec86001d2e8fb8200c71b2f402784ecedf070a11
konevskiysemyon <konevskiy7on@gmail.com>
2026-04-20 10:20:35 +0400

    feat: added a session saving/restore

---
+++ lua/plugins/sessions.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "folke/persistence.nvim",
+    enabled = false,
+  },
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+    opts = {
+      auto_save = true,
+      auto_create = true,
+      auto_restore = true,
+      args_allow_files_auto_save = true,
+      git_use_branch_name = true,
+      git_auto_restore_on_branch_change = true,
+      bypass_save_filetypes = { "alpha", "dashboard", "snacks_dashboard" },
+      suppressed_dirs = { "~/", "~/Downloads", "/" },
+    },
+    keys = {
+      { "<leader>qs", "<cmd>AutoSession restore<cr>", desc = "Restore Session" },
+      { "<leader>qS", "<cmd>AutoSession search<cr>", desc = "Select Session" },
+      { "<leader>qw", "<cmd>AutoSession save<cr>", desc = "Save Session" },
+      { "<leader>qd", "<cmd>AutoSession disable<cr>", desc = "Don't Save Current Session" },
+      { "<leader>qD", "<cmd>AutoSession delete<cr>", desc = "Delete Session" },
+      { "<leader>qt", "<cmd>AutoSession toggle<cr>", desc = "Toggle Session Autosave" },
+    },
+  },
+}
