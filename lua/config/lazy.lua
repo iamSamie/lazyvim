1c1b5c788e2fad6b85b413c9704e72066985c04d
konevskiysemyon <konevskiy7on@gmail.com>
2026-04-25 12:37:39 +0400

    added go support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.editor.neo-tree" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,10 @@ return {
     opts = function(_, opts)
       opts.auto_install = true
       opts.ensure_installed = opts.ensure_installed or {}
+
+      if not vim.tbl_contains(opts.ensure_installed, "gotmpl") then
+        table.insert(opts.ensure_installed, "gotmpl")
+      end
     end,
   },
 }
